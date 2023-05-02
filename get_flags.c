fa9c2f5cc6994643b00c01769ba718a7d2f1c79e
pinklad <stevenswawerus@gmail.com>
2023-05-02 21:25:53 +0300

    Every time that I wanted to give up, if I saw an interesting textile, print what ever ,suddenly I would see a collection

--- get_flags.c
+++ get_flags.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_flags - Calculates flags active 
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
- * 
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
